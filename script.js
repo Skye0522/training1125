e8ba0907e6fe2f257bdc2c9731f11455be75d6a7
Skye0522 <tnochiyama@dl-techx-xedge.net>
2025-11-25 16:56:53 +0900

    サインアップ修正2

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Axios共通設定
 // ==============================
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "https://m3h-nochiyama-aca-vnet.proudwater-40accaea.japaneast.azurecontainerapps.io/api"; // バックエンドのカスタムドメイン
+axios.defaults.baseURL = "backapp-nochiyama.m3harbor.net/api"; // バックエンドのカスタムドメイン
 
 // ==============================
 // Vueアプリ設定
